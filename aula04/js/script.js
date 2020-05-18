754b0c1120603220b8f4116a92c0a81211efea62
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-05-18 17:11:46 -0300

    Estruturas de decisão

--- aula04/js/script.js
+++ aula04/js/script.js
@@ -1,4 +1,40 @@
-console.log("Olá, mundo");
+var a = 5;
+var b = 6;
 
-var tittle = document.querySelector("h1");
-tittle.textContent = "Modificado por Mateus Bezzan";
+if (a > b) {
+  console.log(a + "é maior que" + b);
+} else {
+  if (a < b) {
+    console.log(a + "é menor que" + b);
+  } else {
+    console.log(a + "é igual que" + b);
+  }
+}
+var r = "";
+
+switch (dia) {
+  case 1:
+    r = "Domingo";
+    break;
+  case 2:
+    r = "Segunda";
+    break;
+  case 3:
+    r = "Terça";
+    break;
+  case 4:
+    r = "Quarta";
+    break;
+  case 5:
+    r = "Quinta";
+    break;
+  case 6:
+    r = "Sexta";
+    break;
+  case 7:
+    r = "Sabado";
+    break;
+  default:
+    r = "dia invalido";
+}
+console.log(r);
